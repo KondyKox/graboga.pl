7c71cf39d31df6ec00705e1a2e4aaf6efc153edf
KondyKox <108412658+KondyKox@users.noreply.github.com>
2022-10-20 20:39:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
     define('DB_SERWER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', 'rumcajs');
     define('DB_NAME', 'mechan_card_game');
 
     $link = mysqli_connect(DB_SERWER, DB_USERNAME, DB_PASSWORD, DB_NAME,);
