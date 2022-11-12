5d5481d93fddbbdc5835ffc62e10c3fd7ef03346
KondyKox <108412658+KondyKox@users.noreply.github.com>
2022-11-13 00:36:17 +0100

    Update cards.js

--- inventory/cards.js
+++ inventory/cards.js
@@ -1,7 +1,7 @@
 // Pokazuje kartę na ekranie po kliknięciu
 function show(img) {
     let dc = document.getElementById('cards');
-    let item = "<div id='res_show' class='item class_red_item' style='background-image:" + img.style.backgroundImage + ";' onclick='sell(this)'></div>";
+    let item = "<div id='res_show' class='item class_red_item' style='background-image:" + img.style.backgroundImage + ";'></div>";
     document.getElementById('cards').innerHTML = "";
     dc.style.display = "";
     $(item).appendTo('#cards');
