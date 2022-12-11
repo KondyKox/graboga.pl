f70f4a4dc20c7d70cb6c211d7c15bbebe069d4f1
Konrad <108412658+KondyKox@users.noreply.github.com>
2022-12-11 18:01:54 +0100

    Add files via upload

--- play/mechan_kombat/game.js
+++ play/mechan_kombat/game.js
@@ -1,21 +1,3 @@
-// Generuje tło z samych kart lokacji
-function background() {
-    let backgroundCards = [];
-    let background;
-
-    for (let i = 0; i < tab.length; i++) {
-        if (tab[i].rarity === "place")
-            backgroundCards.push(tab[i].photo);
-    }
-
-    background = Math.round(Math.random() * backgroundCards.length);
-
-    document.body.style.backgroundImage = 'url("../' + backgroundCards[background] + '")';
-    //$('#menu').css('backgroundImage', '"../' + backgroundCards[background] + '"');
-}
-
-// ** --------------------------------------------------------------------------------------------------
-
 // Pole walki
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
@@ -25,71 +7,19 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-// Klasa do player i enemy
 const gravity = 0.7;
 
-class Sprite {
-    constructor({ position, velocity, color, offset }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 120;
-        this.height = 200;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset: offset,
-
-            width: 100,
-            height: 50,
-        }
-        this.color = color;
-        this.isAttacking = false;
-        this.health = 100;
-    }
-
-    // Postacie
-    draw() {
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-        // attack box
-        if (this.isAttacking) {
-            c.fillStyle = 'green';
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
-        }
-    }
-
-    // ** Update
-    update() {
-        this.draw();
-
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height)
-            this.velocity.y = 0;
-
-        else 
-            this.velocity.y += gravity;
-    }
-
-    // Atakowanie
-    attack() {
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 100);
-    }
-}
+// Robi tło
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: '../../img/idol_blogoslawi.png'
+});
 
 //Obiekt player
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -100,18 +30,19 @@ const player = new Sprite({
         y: 0
     },
 
-    color: 'red',
-
     offset: {
         x: 0,
         y: 0
     },
+
+    imageSrc: '../../img/cards/kartaMucha.png',
+    scale: 0.3,
 });
 
 player.draw();
 
 //Obiekt enemy
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
@@ -122,12 +53,13 @@ const enemy = new Sprite({
         y: 0
     },
 
-    color: 'blue',
-
     offset: {
         x: -50,
         y: 0
     },
+
+    imageSrc: '../../img/cards/kartaMucha2.png',
+    scale: 0.3,
 });
 
 enemy.draw();
@@ -159,45 +91,7 @@ const keys = {
     },
 }
 
-function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
-        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width 
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-        && rectangle1.isAttacking
-    );
-}
-
-function determineWinner({player, enemy, timerId}) {
-    clearTimeout(timerId);
-    document.querySelector("#displayText").style.display = "flex";
-    // Remis
-    if (player.health === enemy.health)
-        document.querySelector("#displayText").innerHTML = "REMIS";
-    // Player wygrywa
-    else if (player.health > enemy.health)
-        document.querySelector("#displayText").innerHTML = "PLAYER 1 WINS";
-    // Enemy wygrywa
-    else if (player.health < enemy.health)
-        document.querySelector("#displayText").innerHTML = "PLAYER 2 WINS";
-}
-
-// Timer
-let timer = 60;
-let timerId;
-function decreaseTimer() {
-    // Liczenie czasu
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000);
-        document.querySelector("#timer").innerHTML = timer;
-        timer--;
-    }
-
-    if (timer === 0)
-        determineWinner({player, enemy, timerId});
-}
-
+// Wywołuje funkcję odliczania czasu z utils.js
 decreaseTimer();
 
 // Poruszanie się
@@ -206,7 +100,8 @@ function animate() {
 
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
-
+    
+    background.update();
     player.update();
     enemy.update();
 
