da931bf4d55c293eb504c92469c081cc621248f6
Konrad <kondykox@gmail.com>
2024-11-01 14:50:21 +0100

    login & register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,67 +1,74 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsLoading(true);
-    setError('');
+    setError("");
 
     try {
-      const res = await fetch('/api/auth/login', {
-        method: 'POST',
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
       });
 
       if (res.ok) {
         const data = await res.json();
-        localStorage.setItem('token', data.token); // Zapisz token JWT do localStorage
-        router.push('/'); // Przekierowanie do chronionej strony po zalogowaniu
+        localStorage.setItem("token", data.token); // Zapisz token JWT do localStorage
+        router.push("/"); // Przekierowanie do chronionej strony po zalogowaniu
       } else {
         const data = await res.json();
-        setError(data.message || 'Login failed');
+        setError(data.message || "Login failed");
       }
     } catch (err) {
-      setError('Something went wrong');
+      setError("Something went wrong");
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <div className="container">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="username"
-          placeholder="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? 'Logging in...' : 'Login'}
+    <div className="mt-2 p-4 flex flex-col justify-center items-center w-full">
+      <h1 className="text-6xl p-4 lg:text-8xl">Login</h1>
+      <form
+        onSubmit={handleLogin}
+        className="flex flex-col justify-center items-center gap-6"
+      >
+        <div className="py-2 flex flex-col justify-center items-center gap-4">
+          <input
+            type="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className="input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="input"
+          />
+        </div>
+        <button type="submit" disabled={isLoading} className="btn">
+          {isLoading ? "Logging in..." : "Login"}
         </button>
       </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p className="text-special mt-2">{error}</p>}
     </div>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,73 +1,81 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
-    const handleRegister = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setIsLoading(true);
-        setError('');
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
 
-        try {
-            const res = await fetch('/api/auth/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, email, password }),
-            });
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
 
-            if (res.ok) {
-                router.push('/login'); // Przekierowanie do strony logowania po rejestracji
-            } else {
-                const data = await res.json();
-                setError(data.message || 'Registration failed');
-            }
-        } catch (err) {
-            setError('Something went wrong');
-        } finally {
-            setIsLoading(false);
-        }
-    };
+      if (res.ok) {
+        router.push("/login"); // Przekierowanie do strony logowania po rejestracji
+      } else {
+        const data = await res.json();
+        setError(data.message || "Registration failed");
+      }
+    } catch (err) {
+      setError("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return (
-        <div className="container">
-            <h1>Register</h1>
-            <form onSubmit={handleRegister}>
-                <input
-                    type="username"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit" disabled={isLoading}>
-                    {isLoading ? 'Registering...' : 'Register'}
-                </button>
-            </form>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
+  return (
+    <div className="mt-2 p-4 flex flex-col justify-center items-center w-full">
+      <h1 className="text-6xl p-4 lg:text-8xl">Register</h1>
+      <form
+        onSubmit={handleRegister}
+        className="flex flex-col justify-center items-center gap-6"
+      >
+        <div className="py-2 flex flex-col justify-center items-center gap-4">
+          <input
+            type="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className="input"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="input"
+          />
         </div>
-    );
+        <button type="submit" disabled={isLoading} className="btn">
+          {isLoading ? "Registering..." : "Register"}
+        </button>
+      </form>
+      {error && <p className="text-special mt-2">{error}</p>}
+    </div>
+  );
 }
