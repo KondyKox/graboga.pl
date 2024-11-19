b5f4bf771a9f89bcf60ac22da83f8575f9a58b31
Konrad <kondykox@gmail.com>
2024-11-19 20:49:43 +0100

    dark bg for legendary cards

--- src/components/card/UnoCard.tsx
+++ src/components/card/UnoCard.tsx
@@ -9,7 +9,7 @@ const UnoCard: React.FC<{ card: UnoCardProps }> = ({ card }) => {
 
   return (
     <div
-      className="flex justify-center items-center rounded overflow-hidden duration-300 ease-in-out hover:-translate-y-3 w-52 h-80"
+      className="flex justify-center items-center rounded overflow-hidden duration-300 ease-in-out hover:-translate-y-3 w-52 h-80 bg-background"
       style={{
         backgroundImage: `url(${location?.background})`,
         backgroundSize: "cover",
