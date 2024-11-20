5fb5ba2625e9090cab09b13055c4b72c20cfee4c
devamor <77830779+devamor@users.noreply.github.com>
2024-11-20 19:07:37 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        MONGODB_URI: 'mongodb+srv://kondykox:CulULgKsaubNuWCf@sigmaprogrammer.wflse.mongodb.net/?retryWrites=true&w=majority&appName=SigmaProgrammer',
+        MONGODB_URI: process.env.MONGO_DB_URI,
         JWT_SECRET: 'OswiecNasPanieMucho',
     },
 };
