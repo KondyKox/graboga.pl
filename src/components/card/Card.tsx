81ee147e467086b176a18e622c4f70a8f7ed9e69
Konrad <kondykox@gmail.com>
2024-12-11 14:43:23 +0100

    fixed card styles

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,16 +2,49 @@ import CardProps from "@/types/CardProps";
 import Image from "next/image";
 import React from "react";
 
-const Card: React.FC<{ card: CardProps }> = ({ card }) => {
+type Rarity = "common" | "rare" | "epic" | "legendary" | "cursed";
+
+const rarityStyles: Record<
+  Rarity,
+  { container: string; header: string; image: string }
+> = {
+  common: {
+    container: "border-common hover:shadow-common",
+    header: "text-common",
+    image: "border-common group-hover:drop-shadow-common",
+  },
+  rare: {
+    container: "border-rare hover:shadow-rare",
+    header: "text-rare",
+    image: "border-rare group-hover:drop-shadow-rare",
+  },
+  epic: {
+    container: "border-epic hover:shadow-epic",
+    header: "text-epic",
+    image: "border-epic group-hover:drop-shadow-epic",
+  },
+  legendary: {
+    container: "border-legendary hover:shadow-legendary",
+    header: "text-legendary",
+    image: "border-legendary group-hover:drop-shadow-legendary",
+  },
+  cursed: {
+    container: "border-cursed hover:shadow-cursed",
+    header: "text-cursed",
+    image: "border-cursed group-hover:drop-shadow-cursed",
+  },
+};
+
+const Card: React.FC<{ card: CardProps & { rarity: Rarity } }> = ({ card }) => {
   const rarity = card.rarity;
 
   return (
     <div
       className={`bg-none rounded px-2 py-4 flex flex-col justify-around items-center w-52 h-80 border-2 transition 
-                  duration-300 ease-in-out group border-${rarity} hover:shadow-${rarity}`}
+                  duration-300 ease-in-out group ${rarityStyles[rarity].container}`}
     >
       <h2
-        className={`text-xl font-bold text-center text-wrap px-2 h-10 text-${rarity}`}
+        className={`text-xl font-bold text-center text-wrap px-2 h-10 ${rarityStyles[rarity].header}`}
       >
         {card.name}
       </h2>
@@ -20,8 +53,7 @@ const Card: React.FC<{ card: CardProps }> = ({ card }) => {
         alt={card.name}
         width={128}
         height={128}
-        className={`rounded border-2 transform transition duration-300 ease-in-out w-28 my-2 border-${rarity} 
-                    group-hover:drop-shadow-${rarity}`}
+        className={`rounded border-2 transform transition duration-300 ease-in-out w-28 my-2 ${rarityStyles[rarity].image}`}
       />
       <p className="text-center text-sm text-wrap italic px-2">
         {card.description}
